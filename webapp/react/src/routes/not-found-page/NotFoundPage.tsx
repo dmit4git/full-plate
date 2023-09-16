ad00753085e1f7ab1e8e171c28bbae3d36b99306
Dmitry <box4dmitry@gmail.com>
2023-09-16 19:04:17 -0300

    FP-1: GHA deploy test

--- webapp/react/src/routes/not-found-page/NotFoundPage.tsx
+++ webapp/react/src/routes/not-found-page/NotFoundPage.tsx
@@ -10,7 +10,7 @@ function NotFoundPageComponent(): ReactElement {
     return(
         <div className="not-found-page flex flex-column align-items-center justify-content-center gap-3">
             <div>
-                <span className="text-4xl text-primary mr-3">Opps</span>
+                <span className="text-4xl text-primary mr-3">Whoops</span>
                 <b className="text-8xl">404</b>
             </div>
             <p className="text-4xl text-primary">Page not found</p>
