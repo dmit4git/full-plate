b6f5196a26fd81f9319c0085afd45ae88c5f72ab
Dmitry <box4dmitry@gmail.com>
2023-11-11 17:58:31 -0400

    FP-2: merge fix

--- webapp/react/src/routes/routes.tsx
+++ webapp/react/src/routes/routes.tsx
@@ -25,6 +25,11 @@ export const routes = [
                 path: "home",
                 element: <Home />,
                 errorElement: <ErrorPage />
+            },
+            {
+                path: "user-permissions",
+                element: <UserPermissions />,
+                errorElement: <ErrorPage />
             }
         ]
     }
