ca950dfd96d011abea5bd26e3fa281a688ee9a2b
Dmitry <box4dmitry@gmail.com>
2024-01-27 22:06:58 -0400

    FP-1: env variables for nginx

--- nginx/certbot/entrypoint.sh
+++ nginx/certbot/entrypoint.sh
@@ -2,8 +2,9 @@
 if [ ! -d /etc/letsencrypt/live ]; then
   certbot certonly --non-interactive \
     --webroot --webroot-path /var/www/certbot/ \
-    --domains "fullplate.dev, www.fullplate.dev, logs.fullplate.dev" \
-    --email change_me@email.com
+    --domains "${CERTBOT_DOMAIN_NAME}, www.${CERTBOT_DOMAIN_NAME}, logs.${CERTBOT_DOMAIN_NAME}" \
+    --email "admin@${CERTBOT_DOMAIN_NAME}" \
+    --agree-tos
 fi
 # renew the certificate every 12 hours
 watch -n 43200 certbot renew --non-interactive
