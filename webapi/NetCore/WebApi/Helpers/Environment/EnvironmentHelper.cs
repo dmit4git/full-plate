247de3f7673057aadae361447fce9aded46c9f2e
Dmitry <box4dmitry@gmail.com>
2024-08-04 16:04:19 -0300

    FP-7: E2E tests cleanup №3

--- webapi/NetCore/WebApi/Helpers/Environment/EnvironmentHelper.cs
+++ webapi/NetCore/WebApi/Helpers/Environment/EnvironmentHelper.cs
@@ -2,36 +2,52 @@ namespace WebApi.Helpers.Environment;
 
 public static class EnvironmentHelper
 {
+    public static bool IsDevelopmentEnvironment()
+    {
+        var aspNetCoreEnvironment = System.Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "";
+        return String.Equals(aspNetCoreEnvironment.ToLower(), "development");
+    }
+    
     public static bool IsE2ETestEnvironment()
     {
         var aspNetCoreEnvironment = System.Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "";
         return String.Equals(aspNetCoreEnvironment.ToLower(), "e2etests");
     }
     
-    public static bool IsDevelopmentEnvironment()
+    public static string? GetEnvironmentVariable(string variableName)
     {
-        var aspNetCoreEnvironment = System.Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "";
-        return String.Equals(aspNetCoreEnvironment.ToLower(), "development");
+        string? variable = System.Environment.GetEnvironmentVariable(variableName);
+        if (variable == String.Empty)
+        {
+            variable = null;
+        }
+
+        if (variable is not null)
+        {
+            Console.WriteLine($"EnvironmentHelper has read an environment variable {variableName}={variable}");
+        }
+
+        return variable;
     }
 
     public static string? GetDomainName()
     {
-        return System.Environment.GetEnvironmentVariable("NGINX_DOMAIN_NAME");
+        return GetEnvironmentVariable("NGINX_DOMAIN_NAME");
     }
     
     public static string? GetDbConnectionStringName()
     {
-        return System.Environment.GetEnvironmentVariable("BACKEND_DATABASE_CONNECTION_STRING_NAME");
+        return GetEnvironmentVariable("BACKEND_DATABASE_CONNECTION_STRING_NAME");
     }
     
     public static string? GetEmailServiceName()
     {
-        return System.Environment.GetEnvironmentVariable("BACKEND_EMAIL_SERVICE");
+        return GetEnvironmentVariable("BACKEND_EMAIL_SERVICE");
     }
     
     public static int? GetHttpPort()
     {
-        string? portString = System.Environment.GetEnvironmentVariable("NGINX_PROXY_HTTP_PORT");
+        string? portString = GetEnvironmentVariable("NGINX_PROXY_HTTP_PORT");
         bool success = Int32.TryParse(portString, out int port);
         if (success)
         {
