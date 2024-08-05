dfb65749dfab2ec3f73897a8245a461383a7bccc
Dmitry <box4dmitry@gmail.com>
2024-08-05 02:13:48 -0300

    FP-8: use KeyCloak for IdP, initial commit

--- nginx/certbot/entrypoint.sh
+++ nginx/certbot/entrypoint.sh
@@ -2,7 +2,7 @@
 if [ ! -d /etc/letsencrypt/live ]; then
   certbot certonly --non-interactive \
     --webroot --webroot-path /var/www/certbot/ \
-    --domains "${CERTBOT_DOMAIN_NAME}, www.${CERTBOT_DOMAIN_NAME}, logs.${CERTBOT_DOMAIN_NAME}, metrics.${CERTBOT_DOMAIN_NAME}" \
+    --domains "${CERTBOT_DOMAIN_NAME}, www.${CERTBOT_DOMAIN_NAME}, logs.${CERTBOT_DOMAIN_NAME}, metrics.${CERTBOT_DOMAIN_NAME}, users.${CERTBOT_DOMAIN_NAME}" \
     --email "admin@${CERTBOT_DOMAIN_NAME}" \
     --agree-tos
 fi
