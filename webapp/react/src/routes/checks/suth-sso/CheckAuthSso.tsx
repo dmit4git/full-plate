86eeae85d94eadcb452566dc2c7e514008938cce
Dmitry <box4dmitry@gmail.com>
2024-09-06 16:58:40 -0300

    FP-8: prettify endpoints protection check

--- webapp/react/src/routes/checks/suth-sso/CheckAuthSso.tsx
+++ webapp/react/src/routes/checks/suth-sso/CheckAuthSso.tsx
@@ -1,56 +1,92 @@
-import React, {ReactElement} from "react";
+import "./CheckAuthSso.scss";
+import React, {ReactElement, RefObject, useRef} from "react";
 import {Button} from "primereact/button";
-import {Message} from "primereact/message";
 import {
     useLazyAuthenticatedEndpointQuery,
     useLazyRoleProtectedEndpointQuery,
     useLazyUnprotectedEndpointQuery
 } from "./CheckAuthSsoApi";
-import {UseLazyQuery} from "@reduxjs/toolkit/dist/query/react/buildHooks";
-import {QueryDefinition} from "@reduxjs/toolkit/query";
+import {LazyQueryTrigger, UseLazyQuery, UseQueryStateResult} from "@reduxjs/toolkit/dist/query/react/buildHooks";
+import {QueryDefinition, QueryResultSelectorResult} from "@reduxjs/toolkit/query";
+import {DataTable} from "primereact/datatable";
+import {Column} from "primereact/column";
+import {isObject} from "../../../helpers/checks";
+import {Messages} from "primereact/messages";
 
-interface AuthCheckerComponentProps {
-    buttonLabel: string,
-    lazyQuery: UseLazyQuery<QueryDefinition<void, any, any, {value: string}>>
-}
+type EndpointQuery = QueryDefinition<void, any, any, {value: string}>;
 
-function AuthCheckerComponent(props: AuthCheckerComponentProps): ReactElement {
-
-    const [trigger, result] = props.lazyQuery();
-
-    function makeMessage() {
-        let message = <></>;
-        if (result.isSuccess) {
-            message = <Message severity='success' text="200" />;
-        } else if (result.isError) {
-            const status = result.error?.status || '???';
-            message = <Message severity='error' text={status} />;
-        }
-        return <div className="w-15rem">{message}</div>;
+class EndpointRow {
+    description: string;
+    queryTrigger: LazyQueryTrigger<EndpointQuery>;
+    queryResult: UseQueryStateResult<EndpointQuery, any>;
+    messagesRef: RefObject<Messages>;
+    messages: ReactElement;
+    constructor(endpointRow: EndpointRow) {
+        this.description = endpointRow.description;
+        this.queryTrigger = endpointRow.queryTrigger;
+        this.queryResult = endpointRow.queryResult;
+        this.messagesRef = endpointRow.messagesRef;
+        this.messages = <Messages className="messages-row" ref={this.messagesRef} />
     }
-
-    return(
-        <div className="flex flex-row gap-4 justify-content-center">
-            <Button className="w-18rem" label={props.buttonLabel}
-                    onClick={() => trigger()}
-                    loading={result.isFetching}/>
-            {makeMessage()}
-        </div>
-    );
 }
-export const AuthChecker = React.memo(AuthCheckerComponent);
 
 function CheckAuthSsoComponent(): ReactElement {
 
+    function getTriggerAndResult(query: UseLazyQuery<EndpointQuery>) {
+        const [queryTrigger, queryResult] = query();
+        return {queryTrigger, queryResult};
+    }
+    const endpointRows: EndpointRow[] = [
+        new EndpointRow({description: "unprotected endpoint",
+            ...getTriggerAndResult(useLazyUnprotectedEndpointQuery),
+            messagesRef: useRef<Messages>(null)} as EndpointRow),
+        new EndpointRow({description: "authenticated endpoint",
+            ...getTriggerAndResult(useLazyAuthenticatedEndpointQuery),
+            messagesRef: useRef<Messages>(null)} as EndpointRow),
+        new EndpointRow({description: "role-protected endpoint",
+            ...getTriggerAndResult(useLazyRoleProtectedEndpointQuery),
+            messagesRef: useRef<Messages>(null)} as EndpointRow)
+    ];
+
+    function callButton(row: EndpointRow) {
+        return <Button className="w-6rem my-3" label="call"
+                       onClick={() => row.queryTrigger().then(result => addQueryResultToStatuses(row, result))}
+                       loading={row.queryResult.isFetching}/>;
+    }
+
+    function addQueryResultToStatuses(row: EndpointRow, result: QueryResultSelectorResult<EndpointQuery>) {
+        let status: string | null = null;
+        if (result.isSuccess) {
+            status = '200';
+        } else if (result.isError) {
+            const error = result.error;
+            if (isObject(error)) {
+                status = error.originalStatus || error.status;
+            }
+        }
+        addMessage(row, status || '???');
+    }
+
+    function addMessage(row: EndpointRow, summary: string) {
+        const messages = row.messagesRef.current;
+        if (messages) {
+            const severity = summary === '200' ? 'success' : 'error';
+            messages.show({severity, summary, sticky: false});
+        }
+    }
+
     return(
         <div className="home h-full flex flex-column gap-4 justify-content-center align-items-center">
-            <AuthChecker buttonLabel={"call unprotected endpoint"}
-                         lazyQuery={useLazyUnprotectedEndpointQuery} />
-            <AuthChecker buttonLabel={"call authenticated endpoint"}
-                         lazyQuery={useLazyAuthenticatedEndpointQuery} />
-            <AuthChecker buttonLabel={"call role-protected endpoint"}
-                         lazyQuery={useLazyRoleProtectedEndpointQuery} />
+            <DataTable value={endpointRows} resizableColumns
+                       tableStyle={{ tableLayout: 'fixed', padding: '2rem' }}>
+                <Column field="description" header="Endpoint description"
+                        style={{width: '20%'}} bodyStyle={{ textAlign: 'center' }}></Column>
+                <Column header="Call button" body={callButton}
+                        bodyStyle={{ textAlign: 'center' }} style={{width: '20%'}}></Column>
+                <Column header="Call result" body={(row) => row.messages}></Column>
+            </DataTable>
         </div>
     );
 }
 export const CheckAuthSso = React.memo(CheckAuthSsoComponent);
+
