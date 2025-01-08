31d4921eda43dd988e7b8614df164f1b2544ce0b
Dmitry <box4dmitry@gmail.com>
2025-01-07 21:06:36 -0400

    FP-11: update Keycloak to v26.0.7

--- keycloak/keycloakify/src/helpers/UrlHelper.ts
+++
@@ -1,7 +0,0 @@
-export function getBaseUrl() {
-    let baseUrl = import.meta.env.BASE_URL;
-    if (baseUrl === '/') { // if it's a storybook
-        baseUrl = 'http://localhost:6006/';
-    }
-    return baseUrl;
-}
